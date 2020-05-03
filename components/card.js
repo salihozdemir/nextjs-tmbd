4575772e90955addf440f3662a6e20c37ab8acec
slhozdemir <salihozdemir94@gmail.com>
2020-05-03 19:45:28 +0300

    card components created

---
+++ components/card.js
@@ -0,0 +1,28 @@
+import Link from 'next/link'
+import slug from 'slug'
+
+const Card = ({ id, title, original_title, release_date, poster_path, overview }) => {
+  return (
+    <div className="card" >
+      <div className="card-img">
+        <img src={`https://image.tmdb.org/t/p/w342${poster_path}`} title={title} alt={title} />
+      </div>
+      <div className="card-body">
+        <Link
+          href="/movie/[slug]"
+          as={`/movie/${slug(title)}-${id}`}
+        >
+          <h2>
+            {original_title}
+          </h2>
+        </Link>
+        <p className="release-data">{release_date}</p>
+        <div className="overview">
+          <p>{overview}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
