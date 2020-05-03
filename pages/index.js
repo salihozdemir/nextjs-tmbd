e0bf8a3455216a9ec2ee935e0e845161b920228a
slhozdemir <salihozdemir94@gmail.com>
2020-05-03 16:20:10 +0300

    react infinite scroll library added

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,18 @@
 import axios from 'axios'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import Link from 'next/link'
 import slug from 'slug'
-import InfiniteScroll from 'react-infinite-scroll-component'
+import InfiniteScroll from 'react-infinite-scroller'
 
 export default () => {
   const [movieList, setMovieList] = useState([])
-  const [page, setPage] = useState(1)
   const [hasMore, setHasMore] = useState(true)
 
-  useEffect(() => {
+  const fetchMore = (page) => {
+    if (page >= 500) {
+      setHasMore(false)
+    }
+
     axios
       .get(
         `https://api.themoviedb.org/3/discover/movie?api_key=148a68d49e546737c3a830d5df60f0d2&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`
@@ -17,39 +20,40 @@ export default () => {
       .then((res) => {
         setMovieList(movieList.concat(res.data.results))
       })
-    return () => {
-      if (page >= 500) {
-        setHasMore(false)
-      }
-    }
-  }, [page])
+  }
 
   return (
-    <InfiniteScroll
-      dataLength={movieList.length}
-      next={() => {
-        setPage((prvPage) => prvPage + 1)
-      }}
-      hasMore={hasMore}
-      scrollThreshold="0px"
-      loader={<h4>Loading...</h4>}
-    >
-      <div>
-        {/* {movieList.map((movie) => (
-          <ul key={movie.id}>
-            <Link
-              // href="/movie/[slug]"
-              // as={`/movie/${slug(movie.title)}-${movie.id}`}
-            >
-              <a className="btn btn-link">
-                <li>
-                  <strong>{movie.original_title}</strong>
-                </li>
-              </a>
-            </Link>
-          </ul>
-        ))} */}
-      </div>
-    </InfiniteScroll>
+    <div className="container">
+      <InfiniteScroll
+        pageStart={1}
+        loadMore={fetchMore.bind(this)}
+        hasMore={hasMore}
+        threshold={1000}
+        loader={<h4>Loading...</h4>}
+      >
+        <div className="row">
+          {movieList.map((movie) => (
+            <div className="col-xs">
+              <div className="card" key={movie.id}>
+                <div className="card-img">
+                  <img src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} />
+                </div>
+                <div className="card-body">
+                  <Link
+                    href="/movie/[slug]"
+                    as={`/movie/${slug(movie.title)}-${movie.id}`}
+                  >
+                    <a className="btn btn-link">
+                      {movie.original_title}
+                    </a>
+                  </Link>
+                </div>
+              </div>
+            </div>
+
+          ))}
+        </div>
+      </InfiniteScroll>
+    </div>
   )
 }
