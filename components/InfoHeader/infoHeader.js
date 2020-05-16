4d0dda037d5c8c4d6e2657d63e1c8d9d2fa853a5
salih <salih.ozdemir@metropolsoftware.com>
2020-05-17 02:42:32 +0300

    details page mobile appereance updated

--- components/InfoHeader/infoHeader.js
+++ components/InfoHeader/infoHeader.js
@@ -21,7 +21,7 @@ const infoHeader = ({ movie }) => {
               src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`}
             />
           </div>
-          <div className="col-sm-9">
+          <div className="col-xs-8 d-xs-none">
             <h2 className="title">
               {movie.title} <span>({movie.release_date.slice(0, 4)})</span>
             </h2>
@@ -40,6 +40,34 @@ const infoHeader = ({ movie }) => {
               </div>
             </div>
             <div className="votechart">
+              <VoteChart
+                voteAverage={movie.vote_average * 10}
+                className={'vote-chart-details'}
+              />
+              <div className="text">
+                User <br /> Score
+              </div>
+            </div>
+          </div>
+          <div className="col-xs-12 col-sm-9">
+            <h2 className="title d-xs-none-o">
+              {movie.title} <span>({movie.release_date.slice(0, 4)})</span>
+            </h2>
+            <div className="info-group d-xs-none-o">
+              <div className="release-date">
+                {moment(movie.release_date).format('MM/DD/YYYY')}
+              </div>
+              <div className="genres">
+                <span>{genres.join(', ')}</span>
+              </div>
+              <div className="runtime">
+                {Math.floor(movie.runtime / 60) +
+                  'h ' +
+                  (movie.runtime % 60) +
+                  'm'}
+              </div>
+            </div>
+            <div className="votechart d-xs-none-o">
               <VoteChart
                 voteAverage={movie.vote_average * 10}
                 className={'vote-chart-details'}
