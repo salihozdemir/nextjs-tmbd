97d3726305950311af40bc34f67d78e3af4ebeeb
salih <salih.ozdemir@metropolsoftware.com>
2020-05-16 18:04:58 +0300

    crew component added

--- components/Crew/crew.js
+++ components/Crew/crew.js
@@ -1,10 +1,29 @@
-
-const crew = () => {
-    return (
-        <div className="container">
-            Im'crew
+const crew = ({ casts }) => {
+  return (
+    <div id="crew">
+      <div className="container">
+        <div className="row middle-xs">
+          {casts.map((cast) => (
+            <div className="col-xs" id={cast.id}>
+              <div className="cast-card">
+                <div className="card-img">
+                  <img
+                    src={`https://image.tmdb.org/t/p/w185${cast.profile_path}`}
+                    title={cast.name}
+                    alt={cast.name}
+                  />
+                </div>
+                <div className="card-body">
+                  <p className="name">{cast.name}</p>
+                  <p className="character">{cast.character}</p>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
-    );
+      </div>
+    </div>
+  )
 }
 
-export default crew;
+export default crew
--- pages/movie/[slug].js
+++ pages/movie/[slug].js
@@ -1,23 +1,27 @@
-import axios from 'axios'
+import axios from 'utils/axios'
 import Crew from 'components/Crew/crew'
 import InfoHeader from 'components/InfoHeader/infoHeader'
 
-const MovieDetail = ({ movie }) => (
+const MovieDetail = ({ movie, casts }) => (
   <div id="detail">
     <InfoHeader movie={movie} />
-    <Crew />
+    <Crew casts={casts} />
   </div>
 )
 
 export async function getServerSideProps(context) {
   const movieId = context.params.slug.split('-').slice(-1)[0]
-  const data = await axios.get(
-    `https://api.themoviedb.org/3/movie/${movieId}?api_key=148a68d49e546737c3a830d5df60f0d2&language=en-US`
-  )
+  const movieData = await axios.get(`movie/${movieId}`, {
+    params: { api_key: process.env.API_KEY }
+  })
+  const creditData = await axios.get(`movie/${movieId}/credits`, {
+    params: { api_key: process.env.API_KEY }
+  })
 
   return {
     props: {
-      movie: data.data
+      movie: movieData.data,
+      casts: creditData.data.cast.filter(c => c.profile_path != null)
     }
   }
 }
