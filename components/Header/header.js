835bbd9fda250430b5a0e8edd6750f1128a2c197
salih <salih.ozdemir@metropolsoftware.com>
2020-05-17 18:31:02 +0300

    header updated

--- components/Header/header.js
+++ components/Header/header.js
@@ -7,25 +7,31 @@ import { FaMoon } from 'react-icons/fa'
 const Header = () => {
   const themeContext = useContext(ThemeContext)
   return (
-    <nav>
-      <div className="nav-brands">
-        <MdMovieFilter className="logo" />
-        <div className="text">My Sample</div>
-      </div>
-      <div className="nav-items">
+    <div className="container">
+      <nav>
         <Link href="/">
-          <a className="nav-item">Movies</a>
+          <a className="nav-brands">
+            <MdMovieFilter className="logo" />
+            <div className="text">Discover</div>
+          </a>
         </Link>
-        <Link href="/tv">
-          <a className="nav-item">TV Shows</a>
-        </Link>
-      </div>
-      <div className="nav-theme">
-        <button onClick={() => themeContext.themeDispatch(themeContext.themeState)}>
-          <FaMoon size="1.6rem" />
-        </button>
-      </div>
-    </nav>
+        <div className="nav-items">
+          <Link href="/">
+            <a className="nav-item">Movies</a>
+          </Link>
+          <Link href="/tv">
+            <a className="nav-item">TV Shows</a>
+          </Link>
+        </div>
+        <div className="nav-theme">
+          <button
+            onClick={() => themeContext.themeDispatch(themeContext.themeState)}
+          >
+            <FaMoon size="1.6rem" />
+          </button>
+        </div>
+      </nav>
+    </div>
   )
 }
 
